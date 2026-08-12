3d5a56b7db72b27f8c8926de1952a71019dd8e1b
Hui Wang <haley@victoriametrics.com>
2026-08-13 00:59:43 +0800

    vmalert: skip a redundant pending state when an alert can be restored to firing

--- app/vmalert/rule/group_test.go
+++ app/vmalert/rule/group_test.go
@@ -484,7 +484,7 @@ func TestFaultyNotifier(t *testing.T) {
 	defer cancel()
 
 	go func() {
-		_ = e.exec(ctx, r, time.Now(), 0, 10)
+		_ = e.exec(ctx, r, time.Now(), 0, 10, nil)
 	}()
 
 	tn := time.Now()
@@ -516,7 +516,7 @@ func TestFaultyRW(t *testing.T) {
 		Rw: &remotewrite.Client{},
 	}
 
-	err := e.exec(context.Background(), r, time.Now(), 0, 10)
+	err := e.exec(context.Background(), r, time.Now(), 0, 10, nil)
 	if err == nil {
 		t.Fatalf("expected to get an error from faulty RW client, got nil instead")
 	}
